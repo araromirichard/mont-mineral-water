f234f98c05555cfa558ce4a723425c47d8bc3669
karosaxy <araromirichard@gmail.com>
2023-06-12 01:20:46 +0100

    fix

--- resources/views/mails/orderPlaced.blade.php
+++ resources/views/mails/orderPlaced.blade.php
@@ -5,99 +5,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mont - New Order Placed</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f7fafc;
-            color: #374151;
-        }
-
-        .container {
-            max-width: 600px;
-            margin: 0 auto;
-            padding: 20px;
-        }
-
-        .logo {
-            max-width: 75px;
-            height: auto;
-        }
-
-        .header {
-            text-align: center;
-            margin-bottom: 30px;
-        }
-
-        .order-details {
-            margin-bottom: 30px;
-        }
-
-        .order-details table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-
-        .order-details th,
-        .order-details td {
-            padding: 10px;
-            text-align: left;
-        }
-
-        .user-details table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-top: 30px;
-        }
-
-        .user-details th,
-        .user-details td {
-            padding: 10px;
-            text-align: left;
-            vertical-align: top;
-        }
-
-        .footer {
-            text-align: center;
-            margin-top: 30px;
-            padding-top: 20px;
-            border-top: 1px solid #e5e7eb;
-            font-size: 14px;
-            color: #68727a;
-        }
-    </style>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.15/dist/tailwind.min.css" rel="stylesheet">
 </head>
 
-<body>
-    <div class="container">
-        <div class="header">
-            <a href="http://localhost">
-                <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+<body class="bg-gray-100">
+    <div class="container mx-auto px-4 py-8">
+        <div class="text-center mb-8">
+            <a href="{{ route('/') }}">
+                <img src="{{ asset('/appLogo.png') }}" class="mx-auto max-w-xs" alt="Mont Logo">
             </a>
         </div>
 
-        <div class="order-details">
-            <h1 style="font-size: 24px; font-weight: bold; margin-bottom: 10px;">New Order Placed</h1>
-            <p style="font-size: 16px; margin-bottom: 20px;">A new order has been placed by {{ $mailObj['user']['first_name'] }}
+        <div class="bg-white rounded-lg shadow-lg px-8 py-6 mb-8">
+            <h1 class="text-2xl font-bold mb-4">New Order Placed</h1>
+            <p class="text-lg mb-6">A new order has been placed by {{ $mailObj['user']['first_name'] }}
                 {{ $mailObj['user']['last_name'] }}.</p>
 
-            <h2 style="font-size: 18px; font-weight: bold; margin-bottom: 10px;">Order Details:</h2>
-            <table>
+            <h2 class="text-xl font-bold mb-4">Order Details:</h2>
+            <table class="w-full mb-6">
                 <tr>
-                    <th>Order ID:</th>
-                    <td>{{ $mailObj['order_number'] }}</td>
+                    <th class="py-2">Order ID:</th>
+                    <td class="py-2">{{ $mailObj['order_number'] }}</td>
                 </tr>
                 <tr>
-                    <th>Order Total:</th>
-                    <td>{{ $mailObj['order_total'] }}</td>
+                    <th class="py-2">Order Total:</th>
+                    <td class="py-2">{{ $mailObj['order_total'] }}</td>
                 </tr>
                 <tr>
-                    <th>Order Items:</th>
-                    <td>
-                        <table>
+                    <th class="py-2">Order Items:</th>
+                    <td class="py-2">
+                        <table class="w-full">
                             @foreach($mailObj['orderItems'] as $orderItem)
                             <tr>
-                                <td>{{ $orderItem['product_name'] }}</td>
-                                <td>Quantity: {{ $orderItem['quantity'] }}</td>
+                                <td class="pr-4">{{ $orderItem['product_name'] }}</td>
+                                <td class="pr-4">Quantity: {{ $orderItem['quantity'] }}</td>
                                 <td>Price: GHS{{ $orderItem['price'] }}</td>
                             </tr>
                             @endforeach
@@ -106,36 +47,36 @@
                 </tr>
             </table>
 
-            <h2 style="font-size: 18px; font-weight: bold; margin-bottom: 10px; margin-top: 30px;">User Details:</h2>
-            <table class="user-details">
+            <h2 class="text-xl font-bold mb-4 mt-6">User Details:</h2>
+            <table class="w-full">
                 <tr>
-                    <th>Name:</th>
-                    <td>{{ $mailObj['user']['first_name'] }} {{ $mailObj['user']['last_name'] }}</td>
+                    <th class="py-2">Name:</th>
+                    <td class="py-2">{{ $mailObj['user']['first_name'] }} {{ $mailObj['user']['last_name'] }}</td>
                 </tr>
                 <tr>
-                    <th>Email:</th>
-                    <td>{{ $mailObj['user']['email'] }}</td>
+                    <th class="py-2">Email:</th>
+                    <td class="py-2">{{ $mailObj['user']['email'] }}</td>
                 </tr>
                 <tr>
-                    <th>Address:</th>
-                    <td>{{ $mailObj['shippingAddress']['address'] }}</td>
+                    <th class="py-2">Address:</th>
+                    <td class="py-2">{{ $mailObj['shippingAddress']['address'] }}</td>
                 </tr>
                 <tr>
-                    <th>Apartment:</th>
-                    <td>{{ $mailObj['shippingAddress']['apartment'] }}</td>
+                    <th class="py-2">Apartment:</th>
+                    <td class="py-2">{{ $mailObj['shippingAddress']['apartment'] }}</td>
                 </tr>
                 <tr>
-                    <th>Region:</th>
-                    <td>{{ $mailObj['shippingAddress']['region'] }}</td>
+                    <th class="py-2">Region:</th>
+                    <td class="py-2">{{ $mailObj['shippingAddress']['region'] }}</td>
                 </tr>
                 <tr>
-                    <th>District:</th>
-                    <td>{{ $mailObj['shippingAddress']['district'] }}</td>
+                    <th class="py-2">District:</th>
+                    <td class="py-2">{{ $mailObj['shippingAddress']['district'] }}</td>
                 </tr>
             </table>
         </div>
 
-        <div class="footer">
+        <div class="text-center text-sm text-gray-600">
             <p>&copy; 2023 Mont Mineral Water. All rights reserved.</p>
         </div>
     </div>
--- resources/views/mails/orderReceived.blade.php
+++ resources/views/mails/orderReceived.blade.php
@@ -29,64 +29,64 @@
 
 <body class="bg-white text-gray-700 font-sans"
     style="box-sizing: border-box; position: relative; -webkit-text-size-adjust: none;">
+    <div class="container mx-auto px-4 py-8">
+        <div class="text-center mb-8">
+            <a href="{{ route('/') }}">
+                <img src="{{ asset('/appLogo.png') }}" class="mx-auto max-w-xs" alt="Mont Logo">
+            </a>
+        </div>
 
-    <table class="w-full" cellpadding="0" cellspacing="0" role="presentation">
-        <tr>
-            <td class="text-center py-6">
-                <a href="http://localhost" class="inline-block">
-                    <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo"
-                        style="max-width: 100%; border: none; height: 75px; max-height: 75px; width: 75px;">
-                </a>
-            </td>
-        </tr>
+        <table class="w-full" cellpadding="0" cellspacing="0" role="presentation">
 
-        <tr>
-            <td class="border-t border-b" style="background-color: #edf2f7;">
-                <table class="inner-body mx-auto max-w-2xl" cellpadding="0" cellspacing="0" role="presentation">
-                    <tr>
-                        <td class="px-4 py-8">
-                            <h1 class="text-xl font-bold mb-4">Order Received</h1>
-                            <p class="text-base leading-relaxed mb-6">Your order has been placed.</p>
-                            <p class="text-base leading-relaxed mb-6">A Mont representative will reach out to this
-                                number
-                                <strong>0864 36763 2633</strong> within 30 minutes.
-                            </p>
-                            <p class="text-base leading-relaxed mb-6">If you don't receive a call from us, please call
-                                us on
-                                <strong>+233 488947 89849</strong>.
-                            </p>
-                            <table class="mb-6 w-full border-collapse">
-                                <tr>
-                                    <th class="border px-4 py-2">Item</th>
-                                    <th class="border px-4 py-2">Quantity</th>
-                                    <th class="border px-4 py-2">Price</th>
-                                </tr>
-                                @foreach($mailObj['orderItems'] as $orderItem)
-                                <tr>
-                                    <td class="border px-4 py-2">{{ $orderItem['product_name'] }}</td>
-                                    <td class="border px-4 py-2">{{ $orderItem['quantity'] }}</td>
-                                    <td class="border px-4 py-2">GHS{{ $orderItem['price'] }}</td>
-                                </tr>
-                                @endforeach
-                            </table>
-                            <p class="text-base leading-relaxed mb-6">Order ID:
-                                <strong>
-                                    {{ $mailObj['order_number'] }}
-                                </strong>
-                            </p>
-                        </td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
 
-        <tr>
-            <td class="text-center py-6">
-                <p class="text-sm text-gray-600">© 2023 Mont Mineral Water. All rights reserved.</p>
-            </td>
-        </tr>
-    </table>
+            <tr>
+                <td class="border-t border-b bg-gray-100">
+                    <table class="inner-body mx-auto max-w-2xl" cellpadding="0" cellspacing="0" role="presentation">
+                        <tr>
+                            <td class="px-4 py-8">
+                                <h1 class="text-2xl font-bold mb-4">Order Received</h1>
+                                <p class="text-base leading-relaxed mb-6">Your order has been placed.</p>
+                                <p class="text-base leading-relaxed mb-6">A Mont representative will reach out to this
+                                    number
+                                    <strong>0864 36763 2633</strong> within 30 minutes.
+                                </p>
+                                <p class="text-base leading-relaxed mb-6">If you don't receive a call from us, please
+                                    call
+                                    us on
+                                    <strong>+233 488947 89849</strong>.
+                                </p>
+                                <table class="mb-6 w-full border-collapse">
+                                    <tr>
+                                        <th class="border px-4 py-2">Item</th>
+                                        <th class="border px-4 py-2">Quantity</th>
+                                        <th class="border px-4 py-2">Price</th>
+                                    </tr>
+                                    @foreach($mailObj['orderItems'] as $orderItem)
+                                    <tr>
+                                        <td class="border px-4 py-2">{{ $orderItem['product_name'] }}</td>
+                                        <td class="border px-4 py-2">{{ $orderItem['quantity'] }}</td>
+                                        <td class="border px-4 py-2">GHS{{ $orderItem['price'] }}</td>
+                                    </tr>
+                                    @endforeach
+                                </table>
+                                <p class="text-base leading-relaxed mb-6">Order ID:
+                                    <strong>
+                                        {{ $mailObj['order_number'] }}
+                                    </strong>
+                                </p>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
 
+            <tr>
+                <td class="text-center py-6">
+                    <p class="text-sm text-gray-600">© 2023 Mont Mineral Water. All rights reserved.</p>
+                </td>
+            </tr>
+        </table>
+    </div>
 </body>
 
 </html>
